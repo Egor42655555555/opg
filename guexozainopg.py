2fa6c9d819795313bedcce4c6594822586b28733
Egor42655555555 <plaksaegor47@gmail.com>
2025-02-03 21:28:09 +0200

    Add files via upload

---
+++ guexozainopg.py
@@ -0,0 +1,55 @@
+import time
+import threading
+class guexozain:
+    print("привет, я человек, меня зовут gue, а это мои питомцы: gueбобр и gueхомяк")
+    
+    count_of_guexozain = 1
+    count_of_guexozain_age = 15
+    
+    
+    def __init__(self, name = guexozain, height = 170):
+     self.height = height
+     self.name = name
+     print ("привет, я guexozain")
+     
+     
+      def __init__(self, age = 15):
+      self.age = age
+     Gue.count_of_guexozain_age += 1
+     print ("привет, я guexozain")
+    
+    def increment_count():
+        global count_of_guexozain_age
+        while True:
+            count_of_guexozain_age += 1
+            print(f"ура, мне исполнилось {count_of_guexozain_age}")
+            time.sleep(600)
+            
+            
+         class guexozain:
+             def __init__(self):
+                 self.height = 170
+                 global count_of_guexozain_age
+                 count_of_guexozain_age = 15
+                 
+                 
+                 def grow(self):
+                     global count_of_guexozain_age
+                     
+                     while count_of_guexozain_age < 170
+                     count_of_guexozain_age += 1
+                     
+                     if self.height < 220:
+                         self.height += 10
+                         else:
+                             self.height = 220
+                             
+                             if count_of_guexozain_age > 100
+                             else:
+                                 count_of_guexozain_age = 100
+                                 print("смерть")
+                                 
+            
+            
+            
+    
