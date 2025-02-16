adf84a7fe0a53fc9f99a2c2bc4b4fe165f92ed96
Egor42655555555 <plaksaegor47@gmail.com>
2025-02-16 19:57:23 +0200

    Add files via upload

---
+++ guestandartpet.py
@@ -0,0 +1,44 @@
+class guestandartpet:
+    self.height = 90
+    self.progress = 15
+    
+     def infostandart(self):
+        print(f"height - {self.height}")
+        print(f"progress - {self.progress}")
+        print(f"voice - {self.voice}")
+        
+        
+        
+        
+class guedog(guestandartpet):
+    self.voice = gav
+    
+     def infostandartdog(self):
+        print(f"height - {self.height}")
+        print(f"progress - {self.progress}")
+        print(f"voice - {self.voice}")
+    
+class guecat(guestandartpet):
+    self.voice = miau
+    
+     def infostandartcat(self):
+        print(f"height - {self.height}")
+        print(f"progress - {self.progress}")
+        print(f"voice - {self.voice}")
+    
+    
+guestandartpet = guestandartpet()
+guestandartpet.info()
+print(guestandart.infostandart)
+
+
+guedog = guedog()
+guedog.info()
+print(guedog.infostandartdog)
+
+
+guecat = guecat()
+guecat.info()
+print(guecat.infostandartcat)
+
+
